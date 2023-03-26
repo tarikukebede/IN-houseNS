8bb4af49be4936b17068bafba7c6a8a22697c25e
tcat <tariku.kebede@telus.com>
2023-03-26 10:26:41 +0300

    Initial commit

---
+++ src/main/java/com/tarikukebede/INhousenavigationsystem/InHouseNavigationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tarikukebede.INhousenavigationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InHouseNavigationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InHouseNavigationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarikukebede/INhousenavigationsystem/InHouseNavigationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarikukebede.INhousenavigationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InHouseNavigationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
